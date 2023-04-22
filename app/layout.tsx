9ca3f4d55442060a2ad31454cc0e0d937a91b8a0
Zach McMullen <zachmcmullen04@gmail.com>
2023-04-21 23:12:54 -0400

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-neutral-800 text-white'>
       <body>{children}</body>
     </html>
   )
